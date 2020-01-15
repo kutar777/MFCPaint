03aaaa0edf5d8fb1ee27b6b4343d48df357504ab
kutar777 <tabi_kutar@yahoo.co.jp>
2020-01-16 03:49:16 +0900

    Add files via upload

---
+++ GameSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GameSettings")]
+public class GameSettings : ScriptableObject
+{
+    [SerializeField] private float time = 180f;
+    public float TimeLimits { get { return time; } }
+
+    [SerializeField] private int chest = 5;
+    public int ChestCount { get { return chest; } }
+
+    [SerializeField] private int treasure = 3;
+    public int TreasureCount { get { return treasure; } }
+}
